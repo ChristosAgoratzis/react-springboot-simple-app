c7ab4acb41428798811e94bd3461f400d5a53a8f
ChristosAgoratzis <c.agoratzis@gmail.com>
2025-09-06 21:37:09 +0000

    backend web config changed

--- springboot-app/src/main/java/com/example/spring_app/config/WebConfig.java
+++ springboot-app/src/main/java/com/example/spring_app/config/WebConfig.java
@@ -14,9 +14,10 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("*")
-                        .allowedHeaders("*");
+                        .allowedOrigins("https://christos-agoratzis-app.eu")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
