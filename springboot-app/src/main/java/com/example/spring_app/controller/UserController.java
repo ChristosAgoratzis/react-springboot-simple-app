7f6357cd28445af6e60b9c7aee957df6eb682b9a
ChristosAgoratzis <c.agoratzis@gmail.com>
2025-09-06 12:55:40 +0000

    java code modified

--- springboot-app/src/main/java/com/example/spring_app/controller/UserController.java
+++ springboot-app/src/main/java/com/example/spring_app/controller/UserController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://frontend/")
+@CrossOrigin(origins = "*")
 public class UserController {
 
     @Autowired
