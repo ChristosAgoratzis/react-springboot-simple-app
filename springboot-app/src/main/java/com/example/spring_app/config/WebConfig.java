d1ef70bfe5a804521b802adaac675eb74212b7ff
ChristosAgoratzis <c.agoratzis@gmail.com>
2025-09-07 14:09:41 +0000

    Cors allowed orgins *

--- springboot-app/src/main/java/com/example/spring_app/config/WebConfig.java
+++ springboot-app/src/main/java/com/example/spring_app/config/WebConfig.java
@@ -14,10 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://christos-agoratzis-app.eu")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedOrigins("*")       
             }
         };
     }
